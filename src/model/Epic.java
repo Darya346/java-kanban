5a7829321a9ad7c4c690da4d4928866b2e4e6c0a
Darya346 <dashkins346@gmail.com>
2025-10-07 20:57:00 +0300

    Исправлены ошибки Checkstyle в классе Subtask

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -11,17 +11,32 @@ public class Epic extends Task {
         this.subtaskIds = new ArrayList<>();
     }
 
-    public List<Integer> getSubtaskIds() { return new ArrayList<>(subtaskIds); }
-    public void addSubtaskId(int subtaskId) { if (!subtaskIds.contains(subtaskId)) subtaskIds.add(subtaskId); }
-    public void removeSubtaskId(int subtaskId) { subtaskIds.remove(Integer.valueOf(subtaskId)); }
-    public void clearSubtasks() { subtaskIds.clear(); }
+    public List<Integer> getSubtaskIds() {
+        return new ArrayList<>(subtaskIds);
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        if (!subtaskIds.contains(subtaskId)) {
+            subtaskIds.add(subtaskId);
+        }
+    }
+
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove(Integer.valueOf(subtaskId));
+    }
+
+    public void clearSubtasks() {
+        subtaskIds.clear();
+    }
 
     @Override
     public void setStatus(Status status) {
         throw new UnsupportedOperationException("Статус эпика рассчитывается автоматически");
     }
 
-    public void updateStatus(Status status) { this.status = status; }
+    public void updateStatus(Status status) {
+        this.status = status;
+    }
 
     @Override
     public String toString() {
